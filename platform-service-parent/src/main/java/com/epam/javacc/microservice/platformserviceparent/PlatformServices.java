6ffc1bd96682373ad9fa4d8a8667e470361edd01
Pavel Papou <Pavel_Papou@epam.com>
2023-09-14 09:15:25 -0400

    Platform maintaince service
     - project structure has added

---
+++ platform-service-parent/src/main/java/com/epam/javacc/microservice/platformserviceparent/PlatformServices.java
@@ -0,0 +1,13 @@
+package com.epam.javacc.microservice.platformserviceparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformServices {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformServices.class, args);
+	}
+
+}
---
+++ platform-service-parent/src/test/java/com/epam/javacc/microservice/platformserviceparent/PlatformServicesTests.java
@@ -0,0 +1,13 @@
+package com.epam.javacc.microservice.platformserviceparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformServicesTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
